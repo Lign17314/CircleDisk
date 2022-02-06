3dc98f8192a347171381eb20a86476357cf6ce31
Lign17314 <52576519+Lign17314@users.noreply.github.com>
2022-02-06 21:18:32 +0800

    Add files via upload

--- clock/helloworld.c
+++ clock/helloworld.c
@@ -48,112 +48,164 @@
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
-#include "xspips.h"	 /* SPI device driver */
-#include "xgpiops.h" //°üº¬PS GPIOµÄº¯Êý
+#include "xgpiops.h"
 #include "sleep.h"
-#define GPIO_DEVICE_ID XPAR_XGPIOPS_0_DEVICE_ID
-#define SPI_DEVICE_ID XPAR_XSPIPS_0_DEVICE_ID
-static XSpiPs SpiInstancePtr;
-u32 SPI_Run(XSpiPs *SpiInstancePtr, u32 *data)
+
+// MISO 45
+// CLK 40
+// CS 42
+static XGpioPs psGpioInstancePtr;
+void lmk_wdata(uint32_t data)
 {
-	int Status;
-	u32 rx_data = 0;
-	u8 tx_data[4];
-	tx_data[3]=*((u8 *)data+0);
-	tx_data[2]=*((u8 *)data+1);
-	tx_data[1]=*((u8 *)data+2);
-	tx_data[0]=*((u8 *)data+3);
-	/* send wite enable */
-	Status = XSpiPs_PolledTransfer(SpiInstancePtr, tx_data, &rx_data, 4);
-	if (Status != XST_SUCCESS)
-	{
-		return XST_FAILURE;
-	}
-	return rx_data;
+    int i = 0;
+    usleep(10);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 1);
+    usleep(10);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 0);
+    usleep(10);
+    for (i = 31; i >= 0; i--)
+    {
+        if (data & (1 << i))
+            XGpioPs_WritePin(&psGpioInstancePtr, 45, 1);
+        else
+            XGpioPs_WritePin(&psGpioInstancePtr, 45, 0);
+        usleep(10);
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 0);
+        usleep(10);
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 1);
+        usleep(10);
+        ;
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 0);
+        usleep(10);
+    }
+    usleep(10);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 1);
+    usleep(10);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 0);
+    XGpioPs_WritePin(&psGpioInstancePtr, 40, 0);
+    XGpioPs_WritePin(&psGpioInstancePtr, 45, 0);
+    usleep(10);
 }
+u32 lmk_read(uint32_t addr)
+{
+    int i = 0;
+    u32 data = 0x1f | (addr << 16), data1 = 0, data2 = 0;
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 1);
+    usleep(10);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 0);
+    usleep(10);
+    // lmk_wdata(0x1f);
+    for (i = 31; i >= 0; i--)
+    {
+        if (data & (1 << i))
+            XGpioPs_WritePin(&psGpioInstancePtr, 45, 1);
+        else
+            XGpioPs_WritePin(&psGpioInstancePtr, 45, 0);
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 0);
+        usleep(10);
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 1);
+        usleep(10);
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 0);
+    }
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 1);
+    usleep(10);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 0);
+    XGpioPs_WritePin(&psGpioInstancePtr, 45, 0);
+    usleep(10);
+    data = 0x1f | (addr << 16);
+    for (i = 31; i >= 0; i--)
+    {
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 1);
+        usleep(10);
+        XGpioPs_WritePin(&psGpioInstancePtr, 40, 0);
+        data2 = data2 | (XGpioPs_ReadPin(&psGpioInstancePtr, 41) << i);
+        usleep(10);
+    }
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 1);
+    usleep(10);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 0);
+    return data2;
+}
+int lmk_init()
+{
+    uint32_t lmk_reg[] = {
+        0x800200C0,
+        0x800000C0,
+        0x00000301,
+        0x00000642,
+        0x00000303,
+        0x00000F04,
+        0x00001905,
+        0x44040006,
+        0x44CC0007,
+        0xC0C60008,
+        0x55555549,
+        0x94C0440A,
+        0x33FD300B,
+        0x1B0C016C,
+        0x2363102D,
+        0x1300000E,
+        0x8000800F,
+        0x01550410,
+        0x000000D8,
+        0x80010019,
+        0x0FA8001A,
+        0x1800011B,
+        0x0010005C,
+        0x0100019D,
+        0x0100019E,
+        0x0000003f};
+
+    XGpioPs_Config *GpioConfigPtr;
+    int i;
+    int xStatus;
+
+    GpioConfigPtr = XGpioPs_LookupConfig(XPAR_PS7_GPIO_0_DEVICE_ID);
+    if (GpioConfigPtr == NULL)
+        return XST_FAILURE;
+    xStatus = XGpioPs_CfgInitialize(&psGpioInstancePtr, GpioConfigPtr, GpioConfigPtr->BaseAddr);
+    if (XST_SUCCESS != xStatus)
+        return 0;
+
+    XGpioPs_SetDirectionPin(&psGpioInstancePtr, 40, 1);
+    XGpioPs_SetOutputEnablePin(&psGpioInstancePtr, 40, 1);
+    XGpioPs_WritePin(&psGpioInstancePtr, 40, 1);
+    XGpioPs_SetDirectionPin(&psGpioInstancePtr, 42, 1);
+    XGpioPs_SetOutputEnablePin(&psGpioInstancePtr, 42, 1);
+    XGpioPs_WritePin(&psGpioInstancePtr, 42, 1);
+    XGpioPs_SetDirectionPin(&psGpioInstancePtr, 45, 1);
+    XGpioPs_SetOutputEnablePin(&psGpioInstancePtr, 45, 1);
+    XGpioPs_WritePin(&psGpioInstancePtr, 45, 1);
+
+    XGpioPs_SetDirectionPin(&psGpioInstancePtr, 41, 0);
+
+    for (i = 0; i < sizeof(lmk_reg) / 4; i++)
+    {
+        lmk_wdata(lmk_reg[i]);
+        if(i==0)usleep(1000);
+        if ((i <= 6) & (i >= 1))
+            lmk_wdata(lmk_reg[i]);
+    }
+    u32 data1;
+    for (i = 0; i <= 0x1f; i++)
+    {
+        data1 = lmk_read(i) | i;
+    }
+    lmk_wdata(0x0f05);
+    lmk_wdata(0x0f05);
+    for (i = 1; i <= 6; i++)
+    {
+        lmk_wdata(lmk_reg[i]);
+        lmk_wdata(lmk_reg[i]);
+    }
+}
+
 int main()
 {
-	uint32_t lmk_reg[28] = {
-			0x80160140 ,
-			0x800000C0 ,
-			0x00000301 ,
-			0x00000962 ,
-			0x00000303 ,
-			0x00000964 ,
-			0x00002585 ,
-			0x04040006 ,
-			0x44CC0007 ,
-			0xC0C00008 ,
-			0x55555549 ,
-			0x9000440A ,
-			0x03F1300B ,
-			0x1B0C016C ,
-			0x2363102D ,
-			0x1300000E ,
-			0x8000800F ,
-			0x01550410 ,
-			0x000000D8 ,
-			0x80010019 ,
-			0x0FA8001A ,
-			0x1800005B ,
-			0x0010065C ,
-			0x0100019D ,
-			0x0100019E ,
-			0x0029001F
-	};
-	int i = 0;
-	int Status;
-	u32 rx_data;
-	XSpiPs_Config *SpiConfig;
+    init_platform();
 
-	/*
-	 * Initialize the SPI driver so that it's ready to use
-	 */
-	SpiConfig = XSpiPs_LookupConfig(SPI_DEVICE_ID);
-	if (NULL == SpiConfig)
-	{
-		return XST_FAILURE;
-	}
-
-	Status = XSpiPs_CfgInitialize(&SpiInstancePtr, SpiConfig,
-								  SpiConfig->BaseAddress);
-	if (Status != XST_SUCCESS)
-	{
-		return XST_FAILURE;
-	}
-
-	/*
-	 * Perform a self-test to check hardware build
-	 */
-	Status = XSpiPs_SelfTest(&SpiInstancePtr);
-	if (Status != XST_SUCCESS)
-	{
-		return XST_FAILURE;
-	}
-
-	/*
-	 * Set the Spi device as a master. External loopback is required.
-	 */
-	XSpiPs_SetOptions(&SpiInstancePtr, XSPIPS_MASTER_OPTION | XSPIPS_FORCE_SSELECT_OPTION);
-
-	XSpiPs_SetClkPrescaler(&SpiInstancePtr, XSPIPS_CLK_PRESCALE_128);
-	/*
-	 * Assert the EEPROM chip select
-	 */
-	u32 data=0;
-	XSpiPs_SetSlaveSelect(&SpiInstancePtr, 0);
-	for (i = 0; i < 26; i++)
-	{
-		data=SPI_Run(&SpiInstancePtr, lmk_reg+i);
-		if(i<=4)data=SPI_Run(&SpiInstancePtr, lmk_reg+i);
-		usleep(3000);
-	}
-	u32 tx_data = 0x000001F;
-	for(i=0;i<20;i++)
-	{
-		tx_data = 0x000001F|(i<<16);
-	data=SPI_Run(&SpiInstancePtr, &tx_data);
-	data=SPI_Run(&SpiInstancePtr, &tx_data);
-	}
-	return 0;
+    print("Hello World\n\r");
+    lmk_init();
+    cleanup_platform();
+    return 0;
 }
